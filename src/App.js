08be913e41e7e49b71d95c91a59946d06ebd6d8b
sireesha <yarlagaddasireesha123@gmail.com>
2024-10-23 11:04:57 -0500

    Update App.js, add Tasklist.js, modify index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
-
+import Tasklist from './Tasklist';
 function App() {
+  const [task,setTask] = useState("");
+  const [list_tasks, set_list_tasks] = useState([]);
+  const changeHandler = e => {
+    setTask(e.target.value)
+  }
+  const submitHandler = e => {
+    e.preventDefault();
+    const newtasks = [...list_tasks,task];
+    set_list_tasks(newtasks);
+    setTask("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center> 
+        <div className = "card">
+          <div className = "card-body">
+            <h5 className = "card-title"> Task Manager </h5>
+            <form onSubmit={submitHandler}>
+              <input type="text" name = "task" value= {task} onChange={changeHandler}/> 
+              <input type="submit" value="Add" name = "Add"/>
+            </form>
+            <Tasklist taskslist={list_tasks}/>
+          </div>
+        </div>
+      </center> 
     </div>
   );
 }
